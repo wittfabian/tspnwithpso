8fccae43c59e5dc8806c9d02c6cb802a92732da0
SgtBlack <fabian.witt@t-online.de>
2015-11-02 15:39:51 +0100

    add all data files to the test program

--- test.m
+++ test.m
@@ -1,6 +1,6 @@
 
 %list of datafiles
-filename = {'_tspn2DE5_1', '_tspn2DE5_2'};
+filename = {'_tspn2DE5_1', '_tspn2DE5_2', '_tspn2DE6_1', '_tspn2DE6_2', '_tspn2DE7_1', '_tspn2DE7_2', '_tspn2DE8_1', '_tspn2DE8_2', '_tspn2DE9_1', '_tspn2DE9_2', '_tspn2DE10_1', '_tspn2DE10_2', '_tspn2DE11_1', '_tspn2DE11_2', '_tspn2DE12_1', '_tspn2DE12_2', '_tspn2DE13_1', '_tspn2DE13_2', '_tspn2DE14_1', '_tspn2DE14_2', '_tspn2DE15_1', '_tspn2DE15_2', '_tspn2DE16_1', '_tspn2DE16_2'};
 
 %set the delimiter
 delimiterIn = ' ';
@@ -9,7 +9,7 @@ delimiterIn = ' ';
 headerlinesIn = 1; 
 
 %loop through the files
-for f=1:1:size(filename,2)
+for f=1:1:1 %size(filename,2)
     
     %load datafile
     datastruct = importdata(['data/' filename{f} '.dat'], delimiterIn, headerlinesIn);
